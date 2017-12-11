f16ec419434304b2b74e9dc562e387d42a0511c9
Eugene <eformanes@gmail.com>
2017-12-10 21:44:27 -0800

    Twitter API now looping through results
    Returning both tweet date and text

--- liri.js
+++ liri.js
@@ -1,34 +1,35 @@
+//  Assign the command name entered from the console for testing
+//  If have time, obtain input using Inquirer
 var commandName = process.argv[2];
+
 var twitterStuff = require("./keys.js");
 var request = require("request");
 var twitter = require("twitter");
 
-if(commandName === 'my-tweets'){
+if(commandName === 'my-tweets'){  
+  //console.log(twitterStuff);
+  var twitterQuery = "https://api.twitter.com/1.1/search/tweets.json?q=";
 
-    console.log(twitterStuff);
-    var twitterQuery = "https://api.twitter.com/1.1/search/tweets.json?q=";
-
-    var twiterClient = new twitter(twitterStuff);
-
+  var twiterClient = new twitter(twitterStuff);
 
 
-    var params = {screen_name: 'eeeDemo123'};
-    twiterClient.get('statuses/user_timeline', params, function(error, tweets, response) {
+
+  var params = {screen_name: 'eeeDemo123'};
+  twiterClient.get('statuses/user_timeline', params, function(error, tweets, response) {
       if (!error) {
-        console.log(tweets);
-        
-        //This works but need to pull just the text out
-        // for(var key in tweets){
-        //     console.log(key.text);
-        // }
+      //console.log(tweets[0].text);
 
-
+      //console.log(tweets[0].created_at);
+      console.log("Last tweets from Twitter user " + params.screen_name);
+      //  Loop through the returned tweets array
+      for(var i=0; i< tweets.length; i++){
+        console.log("----------------");
+        console.log("Tweet created " + tweets[i].created_at);
+        console.log("Tweet text: " + tweets[i].text);
       }
-    });
 
-
-
- 
+    }
+  });
 
 }
 
