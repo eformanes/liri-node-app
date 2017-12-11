38e0bff19d07f3274c28ba3d2841e99f1f5e1ce3
Eugene <eformanes@gmail.com>
2017-12-11 01:49:53 -0800

    Spotify Keys Updated and Spotify search functional

--- keys.js
+++ keys.js
@@ -7,4 +7,13 @@ var twitterKeys = {
   access_token_secret: 'FyiNJCvu5AT1YrQLzzWfV0xdjBVYZiMcDQWShbevEJiMR',
 }
 
-module.exports = twitterKeys;
+var spotifyKeys = {
+  id: 'ee50fb27f48348539981a13bc7e77536',
+  secret:'80c085ae450047a4a5f0b4d65b5a9b02'
+}
+
+
+// Exports an array of keys  
+//  Twitter keys are index 0
+//  Spotify keys are index 1
+module.exports = [twitterKeys, spotifyKeys];
--- liri.js
+++ liri.js
@@ -1,14 +1,44 @@
 //  Assign the command name entered from the console for testing
 //  If have time, obtain input using Inquirer
 var commandName = process.argv[2];
+//var secondSearchParam = process.argv[3];
 
-var twitterStuff = require("./keys.js");
+// Create an empty variable for holding the search parameters
+var searchParameters = "";
+
+// Loop through all the words in the node argument
+// And do a little for-loop magic to handle the inclusion of "+"s
+for (var i = 3; i < process.argv.length; i++) {
+
+  if (i > 3 && i < process.argv.length) {
+
+    searchParameters = searchParameters + "+" + process.argv[i];
+
+  }
+
+  else {
+
+    searchParameters += process.argv[i];
+
+  }
+}
+
+
+
+//"Import external libraries and files"
+// keys file contains all the keys required for this project in an array
+var keys = require("./keys.js");
+// Twitter keys are stored in the first index of the keys array
+var twitterStuff = keys[0];
+var spotifyStuff = keys[1]
+
 var request = require("request");
 var twitter = require("twitter");
+var Spotify = require('node-spotify-api');
 
 if(commandName === 'my-tweets'){  
   //console.log(twitterStuff);
-  var twitterQuery = "https://api.twitter.com/1.1/search/tweets.json?q=";
+  //var twitterQuery = "https://api.twitter.com/1.1/search/tweets.json?q=";
 
   var twiterClient = new twitter(twitterStuff);
 
@@ -29,6 +59,39 @@ if(commandName === 'my-tweets'){
       }
 
     }
+  });
+
+}
+else if(commandName === 'spotify-this-song'){
+  console.log(spotifyStuff);
+  var spotify = new Spotify(spotifyStuff);
+
+
+  spotify.search({ type: 'track', query: searchParameters }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+   
+    // data.tracks.items is an array
+    //below returns artists name
+    //console.log(data.tracks.items[0].album.artists[0].name); 
+    
+    // below returns name of song
+    //console.log(data.tracks.items[0].name); 
+
+    // below returns link to album
+    //console.log(data.tracks.items[0].album.external_urls);
+    
+    // below returns name of the album
+    //console.log(data.tracks.items[0].album.name);
+    
+    console.log("Spotify Search Parameters are " + searchParameters);
+    console.log("----------------");
+    console.log("Artist name is: " + data.tracks.items[0].album.artists[0].name); 
+    console.log("Name of song is: " + data.tracks.items[0].name);
+    console.log("Link to album preview: " + data.tracks.items[0].album.external_urls);
+    console.log("Name of the album is: " + data.tracks.items[0].album.name);
+
   });
 
 }
